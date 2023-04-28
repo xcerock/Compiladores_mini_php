2daba9d3c3b3edf8693e92014f8e433502bd4c2c
xcerock <frederick.castaneda@utp.edu.co>
2023-04-27 22:50:20 -0500

    ultimas correciones

--- test.php
+++ test.php
@@ -2,48 +2,5 @@
 require 'somefile.php';
 $var = 'Roberto';
 $Var = 'Juan';
-$_4site = 'aun no';     // válido; comienza con un carácter de subrayado
-# Bryan Steven Cardona
-# Andres Manuel Prieto
-# Juan Esteban Velasquez
-
-if ($left > $right)
-    {
-        return 1;
-    }
-
-echo "ger SDASJAKLX AXANKJXA";
-
-
-# Este es un comentario de una linea
-function RecursiveBinarySearch()
-// Este es otro comentario de una linea
-{
-    echo "ger SDASJAKLX AXANKJXA";
-    if ($left > $right)
-    {
-        return 1;
-    }
-    /* Este es un comentario de
-    varias lineas*/
-
-    if ($comparision_result == 0)
-    {
-        return $average_element_index;
-    }
-    else
-    {
-        if ($comparision_result > 0)
-        {
-            $left = $average_element_index;
-            return RecursiveBinarySearch($array, $search, $left, $right);
-        }
-        else
-        {
-            $right = $average_element_index;
-            return RecursiveBinarySearch($array, $search, $left, $right);
-        }
-    }
-}
-
+$_4site = 'aun no';     
 ?>
